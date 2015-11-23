c039c32d7e34cc79b40d1ef243affbb3bf78a721
nitram509 <maki@bitkings.de>
2015-11-24 00:10:57 +0100

    simple Exception based error handling

--- jbrotli/src/main/java/de/bitkings/jbrotli/BrotliDeCompressor.java
+++ jbrotli/src/main/java/de/bitkings/jbrotli/BrotliDeCompressor.java
@@ -4,19 +4,19 @@ import java.nio.ByteBuffer;
 
 public final class BrotliDeCompressor {
 
-  public final int deCompress(byte[] in, byte[] out) {
+  public final int deCompress(byte[] in, byte[] out) throws BrotliDecompressException {
     return deCompress(in, 0, in.length, out);
   }
 
-  public final int deCompress(byte[] in, int inPosition, int inLength, byte[] out) {
-    return deCompressBytes(in, inPosition, inLength, out);
+  public final int deCompress(byte[] in, int inPosition, int inLength, byte[] out) throws BrotliDecompressException {
+    return checkDecompressionResult(deCompressBytes(in, inPosition, inLength, out));
   }
 
-  public final int deCompress(ByteBuffer in, ByteBuffer out) {
+  public final int deCompress(ByteBuffer in, ByteBuffer out) throws BrotliDecompressException {
     return deCompress(in, 0, in.limit(), out);
   }
 
-  public final int deCompress(ByteBuffer in, int inPosition, int inLength, ByteBuffer out) {
+  public final int deCompress(ByteBuffer in, int inPosition, int inLength, ByteBuffer out) throws BrotliDecompressException {
     int pos = in.position();
     int limit = in.limit();
     assert (pos <= limit);
@@ -27,15 +27,25 @@ public final class BrotliDeCompressor {
     if (in.isDirect() && out.isDirect()) {
       outLength = deCompressByteBuffer(in, in.position(), inLength, out);
     } else if (in.hasArray() && out.hasArray()) {
-      outLength = deCompressBytes(in.array(), pos + in.arrayOffset(), rem, out.array());
+      outLength = checkDecompressionResult(deCompressBytes(in.array(), pos + in.arrayOffset(), rem, out.array()));
       out.limit(pos + outLength);
     } else {
       byte[] b = new byte[rem];
       in.get(b);
-      outLength = deCompressBytes(b, 0, b.length, out.array());
+      outLength = checkDecompressionResult(deCompressBytes(b, 0, b.length, out.array()));
     }
     in.position(limit);
     return outLength;
+  }
+
+  private int checkDecompressionResult(int brotliResult) {
+    if (brotliResult == -1) throw new BrotliDecompressException("An error happened inside JNI call. Maybe OOME or other issues.");
+    if (brotliResult == -10) throw new BrotliDecompressException("Decoding error, e.g. corrupt input or no memory.");
+    if (brotliResult == -12)
+      throw new BrotliDecompressException("Partially done, but must be called again with more input.");
+    if (brotliResult == -13)
+      throw new BrotliDecompressException("Partially done, but must be called again with more output.");
+    return brotliResult;
   }
 
   private native static int deCompressBytes(byte[] in, int inPosition, int inLength, byte[] out);
