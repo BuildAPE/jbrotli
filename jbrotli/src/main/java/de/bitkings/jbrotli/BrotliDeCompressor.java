53f9be53b594bda285ff103c6e556e9f3530d319
nitram509 <maki@bitkings.de>
2015-11-24 00:12:52 +0100

    rename camel case part 1of2

--- jbrotli/src/main/java/de/bitkings/jbrotli/BrotliDeCompressor.java
+++ jbrotli/src/main/java/de/bitkings/jbrotli/BrotliDeCompressor.java
@@ -4,19 +4,19 @@ import java.nio.ByteBuffer;
 
 public final class BrotliDeCompressor {
 
-  public final int deCompress(byte[] in, byte[] out) throws BrotliDecompressException {
+  public final int deCompress(byte[] in, byte[] out) throws BrotliDe_compressException {
     return deCompress(in, 0, in.length, out);
   }
 
-  public final int deCompress(byte[] in, int inPosition, int inLength, byte[] out) throws BrotliDecompressException {
+  public final int deCompress(byte[] in, int inPosition, int inLength, byte[] out) throws BrotliDe_compressException {
     return checkDecompressionResult(deCompressBytes(in, inPosition, inLength, out));
   }
 
-  public final int deCompress(ByteBuffer in, ByteBuffer out) throws BrotliDecompressException {
+  public final int deCompress(ByteBuffer in, ByteBuffer out) throws BrotliDe_compressException {
     return deCompress(in, 0, in.limit(), out);
   }
 
-  public final int deCompress(ByteBuffer in, int inPosition, int inLength, ByteBuffer out) throws BrotliDecompressException {
+  public final int deCompress(ByteBuffer in, int inPosition, int inLength, ByteBuffer out) throws BrotliDe_compressException {
     int pos = in.position();
     int limit = in.limit();
     assert (pos <= limit);
@@ -39,12 +39,12 @@ public final class BrotliDeCompressor {
   }
 
   private int checkDecompressionResult(int brotliResult) {
-    if (brotliResult == -1) throw new BrotliDecompressException("An error happened inside JNI call. Maybe OOME or other issues.");
-    if (brotliResult == -10) throw new BrotliDecompressException("Decoding error, e.g. corrupt input or no memory.");
+    if (brotliResult == -1) throw new BrotliDe_compressException("An error happened inside JNI call. Maybe OOME or other issues.");
+    if (brotliResult == -10) throw new BrotliDe_compressException("Decoding error, e.g. corrupt input or no memory.");
     if (brotliResult == -12)
-      throw new BrotliDecompressException("Partially done, but must be called again with more input.");
+      throw new BrotliDe_compressException("Partially done, but must be called again with more input.");
     if (brotliResult == -13)
-      throw new BrotliDecompressException("Partially done, but must be called again with more output.");
+      throw new BrotliDe_compressException("Partially done, but must be called again with more output.");
     return brotliResult;
   }
 
