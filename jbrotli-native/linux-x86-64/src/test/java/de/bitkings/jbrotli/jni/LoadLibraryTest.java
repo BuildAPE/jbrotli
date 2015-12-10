d4cec7da78f81be5280bb4b9928d75a6a818d843
nitram509 <maki@bitkings.de>
2015-12-10 22:03:38 +0100

    add build script for linux 64bit

---
+++ jbrotli-native/linux-x86-64/src/test/java/de/bitkings/jbrotli/jni/LoadLibraryTest.java
@@ -0,0 +1,12 @@
+package de.bitkings.jbrotli.jni;
+
+import org.scijava.nativelib.NativeLoader;
+import org.testng.annotations.Test;
+
+public class LoadLibraryTest {
+
+  @Test
+  public void the_library_can_be_loaded() throws Exception {
+    NativeLoader.loadLibrary("brotli");
+  }
+}
