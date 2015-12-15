7f684d8e09600883c9880c0aa2d3a8a60a49316f
nitram509 <maki@bitkings.de>
2015-12-15 22:45:04 +0100

    cleanup test setup

--- jbrotli/src/test/java/de/bitkings/jbrotli/BrotliCompressorExtremeTest.java
+++ jbrotli/src/test/java/de/bitkings/jbrotli/BrotliCompressorExtremeTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BrotliCompressorExtremeTest {
 
   private BrotliCompressor compressor;
@@ -22,18 +24,22 @@ public class BrotliCompressorExtremeTest {
   @Test(expectedExceptions = BrotliException.class,
       expectedExceptionsMessageRegExp = ".*\\(Error code: -14\\)")
   public void compress_huge_buffer() throws Exception {
+    byte[] inBuf = createArrayWithNumbers(100000);
+    assertThat(inBuf.length).isEqualTo(488890);
 
-    StringBuilder bigData = new StringBuilder();
-    for (int i = 0; i < 100000; i++) {
-      bigData.append(i);
-    }
-    byte[] inBuf = bigData.toString().getBytes();
-    System.out.println(inBuf.length);
     byte[] compressedBuf = new byte[2048];
 
     int outLength = compressor.compress(Brotli.DEFAULT_PARAMETER, inBuf, compressedBuf);
 
     // expected Exception, because output buffer too small
+  }
+
+  private byte[] createArrayWithNumbers(int numberCount) {
+    StringBuilder bigData = new StringBuilder();
+    for (int i = 0; i < numberCount; i++) {
+      bigData.append(i);
+    }
+    return bigData.toString().getBytes();
   }
 
 }
