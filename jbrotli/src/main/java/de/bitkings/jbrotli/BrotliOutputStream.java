497535dba3a6eec4c83aa6afbb040e2fad7fe666
nitram509 <maki@bitkings.de>
2015-12-20 21:38:43 +0100

    get rid of wrapped buffers in BrotliOutputStream

--- jbrotli/src/main/java/de/bitkings/jbrotli/BrotliOutputStream.java
+++ jbrotli/src/main/java/de/bitkings/jbrotli/BrotliOutputStream.java
@@ -2,9 +2,6 @@ package de.bitkings.jbrotli;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-
-import static java.nio.ByteBuffer.allocateDirect;
 
 public class BrotliOutputStream extends OutputStream {
 
@@ -22,39 +19,24 @@ public class BrotliOutputStream extends OutputStream {
 
   @Override
   public void write(int i) throws IOException {
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1);
-    byteBuffer.put((byte) (i & 0xff));
-    byteBuffer.position(0);
-    outputStream.write(asByteArray(brotliStreamCompressor.compress(byteBuffer, false)));
+    byte[] buf = new byte[]{(byte) (i & 0xff)};
+    outputStream.write(brotliStreamCompressor.compress(buf, false));
   }
 
   @Override
   public void write(byte[] b) throws IOException {
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(b.length);
-    byteBuffer.put(b);
-    byteBuffer.position(0);
-    outputStream.write(asByteArray(brotliStreamCompressor.compress(byteBuffer, false)));
+    outputStream.write(brotliStreamCompressor.compress(b, false));
   }
 
   @Override
   public void write(byte[] b, int off, int len) throws IOException {
-    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(b.length);
-    byteBuffer.put(b);
-    byteBuffer.position(off);
-    byteBuffer.limit(len);
-    outputStream.write(asByteArray(brotliStreamCompressor.compress(byteBuffer, false)));
+    outputStream.write(brotliStreamCompressor.compress(b, off, len, false));
   }
 
   @Override
   public void flush() throws IOException {
-    outputStream.write(asByteArray(brotliStreamCompressor.compress(allocateDirect(0), true)));
+    outputStream.write(brotliStreamCompressor.compress(new byte[0], true));
     outputStream.flush();
-  }
-
-  private byte[] asByteArray(ByteBuffer compressed) {
-    byte[] buffer = new byte[compressed.capacity()];
-    compressed.get(buffer);
-    return buffer;
   }
 
   @Override
